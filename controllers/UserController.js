3756727d559ab6e478252ef398bbc025b221a4c3
ArthurSimplicio <arthurpark20@gmail.com>
2025-09-22 14:30:14 -0300

    Icnluindo funcao getAll

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,69 +2,78 @@ import bcrypt from 'bcrypt'
 import { User } from '../models/User.js'
 
 class UserController {
-    static async register(req, res) {
-     const {
-            name,
-            email,
-            phone,
-            password,
-            confirmpassword
-        } = req.body
+   static async register(req, res) {
+      const {
+         name,
+         email,
+         phone,
+         password,
+         confirmpassword
+      } = req.body
 
-     //Validacoes
-     if(!name){
-        res.status(422).json({message: 'Nome obrigatório'})
-        return
-     }
-     if(!email){
-        res.status(422).json({message: 'Email obrigatório'})
-        return
-     }
-     if(!phone){
-        res.status(422).json({message: 'Numero obrigatório'})
-        return
-     }
-     if(!password){
-        res.status(422).json({message: 'Senha obrigatória'})
-        return
-     }
-     if(!confirmpassword){
-        res.status(422).json({message: "Confirme a senha"})
-        return
-     }
-     if(confirmpassword !== password){
-        res.status(422).json({message: 'Senha e confirmacao devem ser iguais'})
-        return
-     }
+      //Validacoes
+      if (!name) {
+         res.status(422).json({ message: 'Nome obrigatório' })
+         return
+      }
+      if (!email) {
+         res.status(422).json({ message: 'Email obrigatório' })
+         return
+      }
+      if (!phone) {
+         res.status(422).json({ message: 'Numero obrigatório' })
+         return
+      }
+      if (!password) {
+         res.status(422).json({ message: 'Senha obrigatória' })
+         return
+      }
+      if (!confirmpassword) {
+         res.status(422).json({ message: "Confirme a senha" })
+         return
+      }
+      if (confirmpassword !== password) {
+         res.status(422).json({ message: 'Senha e confirmacao devem ser iguais' })
+         return
+      }
 
-     const userExists = await User.findOne({email})
+      const userExists = await User.findOne({ email })
 
-     if(userExists){
-        res.status(422).json({message: 'Use outro email por favor!'})
-        return
-     }
+      if (userExists) {
+         res.status(422).json({ message: 'Use outro email por favor!' })
+         return
+      }
 
-     const salt = await bcrypt.genSalt(12)
-     const passwordHash = await bcrypt.hash(password, salt)
+      const salt = await bcrypt.genSalt(12)
+      const passwordHash = await bcrypt.hash(password, salt)
 
-     const user = new User({
-        name,
-        email,
-        phone,
-        password: passwordHash
-     })
+      const user = new User({
+         name,
+         email,
+         phone,
+         password: passwordHash
+      })
 
-     try {
-        const newUser = await user.save()
-        res.status(201).json({
+      try {
+         const newUser = await user.save()
+         res.status(201).json({
             message: "Usuario criado",
             newUser
-        })
-     } catch (error) {
-        res.status(500).json({message: error})
-     }
+         })
+      } catch (error) {
+         res.status(500).json({ message: error })
+      }
 
-    }
+   }
+
+   static async getAll(req, res) {
+      try {
+         const users = await User.find();
+         res.status(200).json({users})
+      } catch (error) {
+         res.status(400).json({ message: error })
+      }
+   }
 }
 
 export { UserController }
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,6 +2,7 @@ import Router from "express"
 import { UserController } from "../controllers/UserController.js"
 
 const UserRoutes = Router()
+UserRoutes.get('/', UserController.getAll)
 UserRoutes.post('/register', UserController.register)
 
 export {UserRoutes}
